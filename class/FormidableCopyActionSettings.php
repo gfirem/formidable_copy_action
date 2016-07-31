6ccf7187e5604732b025607a80a3cda17c903b25
gfirem@gmail.com <gfirem@gmail.com>
2016-07-30 22:48:21 -0400

    Upgrading setting view. Correcting admin URL to get destination form.

--- class/FormidableCopyActionSettings.php
+++ class/FormidableCopyActionSettings.php
@@ -20,26 +20,34 @@ class FormidableCopyActionSettings {
 		$gManager = GManagerFactory::buildManager('FormidableCopyActionManager', 'formidable_copy_action', FormidableCopyActionManager::getShort());
 		$key  = get_option( FormidableCopyActionManager::getShort() . 'licence_key' );
 		?>
-		<h3 class="frm_first_h3"><?= FormidableCopyActionManager::t( "Licence Data" ) ?></h3>
+		<h3 class="frm_first_h3"><?= FormidableCopyActionManager::t( "Licence Data for Copy Action" ) ?></h3>
 		<table class="form-table">
-			<tr class="form-field" valign="top">
-				<td width="150px"><label for="key"><?= FormidableCopyActionManager::t( "Order Key" ) ?></label></td>
-				<td><input type="text" name="key" id="key" value="<?= $key ?>"/> <?= FormidableCopyActionManager::t( "Key status: " ).$gManager->getStatus() ?></td>
-			</tr>
 			<tr>
-				<td colspan="2">
-					<span ><?= FormidableCopyActionManager::t("Order key send to you with order confirmation, to get updates.") ?></span>
+				<td width="150px"><?= FormidableCopyActionManager::t( "Version: " ) ?></td>
+				<td>
+					<span><?= FormidableCopyActionManager::getVersion() ?></span>
 				</td>
+			</tr>
+			<tr class="form-field" valign="top">
+				<td width="150px">
+					<label for="key"><?= FormidableCopyActionManager::t( "Order Key: " ) ?></label>
+					<span class="frm_help frm_icon_font frm_tooltip_icon" title="" data-original-title="<?= FormidableCopyActionManager::t( "Order key send to you with order confirmation, to get updates." ) ?>"></span>
+				</td>
+				<td><input type="text" name="<?= FormidableCopyActionManager::getShort() ?>_key" id="<?= FormidableCopyActionManager::getShort() ?>_key" value="<?= $key ?>"/></td>
+			</tr>
+			<tr class="form-field" valign="top">
+				<td width="150px"><?= FormidableCopyActionManager::t( "Key status: " ) ?></label></td>
+				<td><?= $gManager->getStatus() ?></td>
 			</tr>
 		</table>
 	<?php
 	}
 
 	public static function process_form() {
-		if ( isset( $_POST['key'] ) && ! empty( $_POST['key'] ) ) {
-			$gManager = GManagerFactory::buildManager('FormidableCopyActionManager', 'formidable_user_list', FormidableCopyActionManager::getShort());
-			$gManager->activate($_POST['key']);
-			update_option( FormidableCopyActionManager::getShort() . 'licence_key', $_POST['key'] );
+		if ( isset( $_POST[ FormidablePatternFieldManager::getShort() . '_key' ] ) && ! empty( $_POST[ FormidablePatternFieldManager::getShort() . '_key' ] ) ) {
+			$gManager = GManagerFactory::buildManager('FormidableCopyActionManager', 'formidable_copy_action', FormidableCopyActionManager::getShort());
+			$gManager->activate($_POST[ FormidablePatternFieldManager::getShort() . '_key' ]);
+			update_option( FormidableCopyActionManager::getShort() . 'licence_key', $_POST[ FormidablePatternFieldManager::getShort() . '_key' ] );
 		}
 		self::display_form();
 	}
